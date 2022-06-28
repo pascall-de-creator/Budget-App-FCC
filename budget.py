9f297aca971712cf2508c351c60d833391acc3c1
pascall de creator <pascall.de.creator@gmail.com>
2022-06-28 16:18:35 +0000

    complete code for test

--- budget.py
+++ budget.py
@@ -1,10 +1,11 @@
-from math import floor
+from math import *
 
 class Category:
     def __init__(self, name):
         self.name = name
         self.ledger = []
         self.total = 0
+        self.total_expenditure = 0
 
     def deposit(self, amount, description = ""):
         self.ledger.append({"amount": amount, "description": description})
@@ -19,6 +20,7 @@ class Category:
     def withdraw(self, amount, description = ""):
         if self.check_funds(amount):
             self.total -= amount
+            self.total_expenditure += amount
             self.ledger.append({"amount": -amount, "description": description})
             return True
         else:
@@ -39,7 +41,9 @@ class Category:
     def transfer(self, amount, category):
         if self.check_funds(amount):
             self.total -= amount
+            self.total_expenditure += amount
             self.ledger.append({"amount": -amount, "description": "Transfer to " + category.name})
+
             category.ledger.append({"amount": amount, "description": "Transfer from " + self.name})
             category.total += amount
             return True
@@ -47,24 +51,78 @@ class Category:
             return False
 
 def create_spend_chart(categories):
-    pass
+    stats = []
+    chart = "Percentage spent by category\n"
+    percent = 100
+    max_category_name = 0
 
+    for category in categories:
+        stats.append({"category": category.name, "percentage": int(round(category.total_expenditure / (category.total + category.total_expenditure) * 100, -1)) }) # round (10)
+
+    while percent >= 0:
+        chart += '{:>3}|'.format(percent)
+        for bar in stats:
+            if len(bar["category"]) > max_category_name:
+                max_category_name = len(bar["category"])
+
+            if bar["percentage"] >= percent:
+                chart += ' o '
+
+        chart += "\n"
+        percent -= 10
+
+    chart += "    " + "-" * (len(stats) ** 2 + 1)
+
+    letter_index = 0
+    while max_category_name >= letter_index:
+        chart += "\n" + "    "
+        for bar in stats:
+            if len(bar["category"]) > letter_index:
+                chart += " " + bar["category"][letter_index] + " "
+            else:
+                chart += "   "
+
+        letter_index += 1
+
+    return chart
 
 food = Category("Food")
-clothing = Category("Clothing")
-rent = Category("Rent")
+entertainment = Category("entertainment")
+business = Category("business")
 
-food.deposit(100000, "initial deposit")
-food.withdraw(20, "Get Breafast")
-food.withdraw(20, "Get Lunch")
-food.withdraw(20, "Get Dinner")
-food.transfer(2000, rent)
+food.deposit(900, "deposit")
+entertainment.deposit(900, "deposit")
+business.deposit(900, "deposit")
 
-rent.withdraw(100, "Pay for the month")
-rent.transfer(100, clothing)
+food.withdraw(105.55)
+entertainment.withdraw(33.40)
+business.withdraw(10.99)
 
-clothing.withdraw(100, "New Drip unlocked")
+print(create_spend_chart([business, food, entertainment]))
 
-print(food.get_balance())
-print(rent.get_balance())
-print(clothing.get_balance())
+# Percentage spent by category
+# 100|
+#  90|
+#  80|
+#  70|    o
+#  60|    o
+#  50|    o
+#  40|    o
+#  30|    o
+#  20|    o  o
+#  10|    o  o
+#   0| o  o  o
+#     ----------
+#      B  F  E
+#      u  o  n
+#      s  o  t
+#      i  d  e
+#      n     r
+#      e     t
+#      s     a
+#      s     i
+#            n
+#            m
+#            e
+#            n
+#            t
